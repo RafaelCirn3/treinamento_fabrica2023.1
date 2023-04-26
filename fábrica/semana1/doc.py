a9b3bd10cf2cddc0004908ad14f18d1b9111795b
Rafael Cirne <125037856+RafaelCirn3@users.noreply.github.com>
2023-04-25 21:59:16 -0300

    Add files via upload
    projetos realizados durante o treinamento na Fábrica de Softwares

---
+++ fábrica/semana1/doc.py
@@ -0,0 +1,27 @@
+# num1 = float(input('digite um valor: '))
+# num2 = float(input('digite um segundo valor: '))
+# print('a soma dos dois valores equivale a: ' ,num1+num2)
+
+# nota1 = float(input('digite sua nota:' ))
+# nota2 = float(input('digite sua nota:' ))
+# nota3 = float(input('digite sua nota:' ))
+# print((nota1+nota2+nota3)/3)
+
+# import random
+# lista = [1, 2, 3, 4]
+# sorteio = random.choice(lista)
+# print('o numero sorteado foi:', sorteio)
+
+# numero= int(input('digite um número: '))
+# if numero % 2==1 :
+#     print("seu numero é impar")
+# else :
+#     print("seu numero é par")
+
+# km = float(input('digite a distância em Km:'))
+# if km >= 200:
+#     print('o valor da passagem será: R$', km * 1.50)
+# else:
+#     print('o valor da passagem será: R$', km * 1.25)
+
+
