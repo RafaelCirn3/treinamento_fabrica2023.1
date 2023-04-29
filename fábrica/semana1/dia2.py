c605b6744fb24f017b10d58ea674052a06bd5cfc
Rafael Cirne <125037856+RafaelCirn3@users.noreply.github.com>
2023-04-28 22:13:21 -0300

    Add files via upload

---
+++ fábrica/semana1/dia2.py
@@ -0,0 +1,11 @@
+# num = float(input("digite um número: "))
+# if num > 0:
+#     print("esse é um número positivo")
+# else:
+#     print("esse é um  número negativo")
+
+# numero= int(input('digite um número: '))
+# if numero % 2==1 :
+#     print("seu numero é impar")
+# else :
+#     print("seu numero é par")
