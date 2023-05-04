6f681bfd34ade43fb084060e81c847d6ab5db860
Rafael Cirne <125037856+RafaelCirn3@users.noreply.github.com>
2023-05-04 17:01:47 -0300

    Add files via upload

--- fábrica/semana1/dia3.py
+++ fábrica/semana1/dia3.py
@@ -1,27 +1,17 @@
-# num1 = float(input('digite um valor: '))
-# num2 = float(input('digite um segundo valor: '))
-# print('a soma dos dois valores equivale a: ' ,num1+num2)
+from datetime import date
 
-# nota1 = float(input('digite sua nota:' ))
-# nota2 = float(input('digite sua nota:' ))
-# nota3 = float(input('digite sua nota:' ))
-# print((nota1+nota2+nota3)/3)
-
-# import random
-# lista = [1, 2, 3, 4]
-# sorteio = random.choice(lista)
-# print('o numero sorteado foi:', sorteio)
-
-# numero= int(input('digite um número: '))
-# if numero % 2==1 :
-#     print("seu numero é impar")
-# else :
-#     print("seu numero é par")
-
-# km = float(input('digite a distância em Km:'))
-# if km >= 200:
-#     print('o valor da passagem será: R$', km * 1.50)
-# else:
-#     print('o valor da passagem será: R$', km * 1.25)
+data1 = float(input("digite seu ano de nascimento: "))
+data2 = date.today().year
+idade = data2-data1
+if idade >= 18:
+    print("Você é maior de idade. Sua idade é:", idade, "anos")
+else:
+    print("Você é menor de idade. Sua idade é:", idade, "anos")
 
 
+salario = float(input("Informe o seu salário: "))
+despesas = float(input("Informe o total de suas despesas: "))
+
+saldo = salario - despesas
+
+print("Seu saldo restante é: R$", saldo)
