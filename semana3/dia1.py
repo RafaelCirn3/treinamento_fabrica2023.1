d1736672830c438723942d873870b49b3450b554
Rafael Cirne <125037856+RafaelCirn3@users.noreply.github.com>
2023-05-10 16:23:34 -0300

    Add files via upload

---
+++ semana3/dia1.py
@@ -0,0 +1,32 @@
+num = (int(input('Digite um número: ')),
+       int(input('Digite outro número: ')),
+       int(input('Digite mais um número: ')),
+       int(input('Digite o último número: ')))
+print(f'Você digitou os valores {num}')
+print(f'O valor 9 apareceu {num.count(9)} vezes')
+if 3 in num:
+    print(f'O valor 3 apareceu na {num.index(3)+1} posição.')
+else:
+    print('O valor 3 não foi digitado em nenhuma posição.')
+print('Os valores pares digitados foram ', end='')
+for n in num:
+    if n % 2 == 0:
+        print(n, end=' ')
+        
+        listagem = ('Lápis', 1.75,
+            'Borracha', 2.00,
+            'Caderno', 15.90,
+            'Estojo', 25.00,
+            'Transferidor', 4.20,
+            'Compasso', 9.99,
+            'Mochila', 120.32,
+            'Canetas', 22.30,
+            'Livro', 34.90)
+print('-' * 40)
+print(f'{"LISTAGEM DE PREÇO":^40}')
+print('-' * 40)
+for pos in range(0, len(listagem)):
+    if pos % 2 == 0:
+        print(f'{listagem[pos]:.<30}', end='')
+    else:
+        print(f'R${listagem[pos]:>7.2f}')
